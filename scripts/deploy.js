181d7ed1506353ef94d19a13905c1e34a795d5f7
Saurabh vishwakarma <saurvishwakarma040207@gmail.com>
2025-10-25 14:19:39 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,3 @@
-
-
 const { ethers } = require("hardhat");
 async function main() {
   console.log("Starting deployment of SupplyChainVerification contract...");
@@ -45,6 +43,7 @@ main()
     console.error(error);
     process.exit(1);
   });
+
 
 
 
